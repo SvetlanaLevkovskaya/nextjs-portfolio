ada12ef17ce0321b8c57bcbe410ba84b94c63d1f
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-08-27 04:29:43 +0300

    feat: change picture

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { Layout } from "@/components/layout";
 import Image from "next/image";
-import profilePic from "../../public/images/profile/developer-pic-1.png";
+import profilePic from "../../public/images/profile/1.png";
 import { AnimatedText } from "@/components/animated-text";
 import Link from "next/link";
 import { LinkArrow } from "@/components/icons";
@@ -16,8 +16,8 @@ export default function Home() {
       <main className="flex items-center text-dark w-full min-h-screen">
         <Layout className="pt-0">
           <div className="flex items-center justify-between w-full">
-            <div className="w-1/2">
-              <Image src={profilePic} alt="image" className="w-full h-auto" />
+            <div className="w-1/3">
+              <Image src={profilePic} alt="image" className="w-full h-auto rounded-l-full" />
             </div>
             <div className="w-1/2 flex flex-col items-center self-center">
               <AnimatedText text="Turning Vision Into Reality With Code And Design." className="!text-6xl !text-left" />
