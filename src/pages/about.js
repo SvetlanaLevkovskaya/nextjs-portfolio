4aa24e987a63d4773ecd5b8922321f560794ff62
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-08-31 00:54:25 +0300

    feat: add responsiveness to about page & skills component

--- src/pages/about.js
+++ src/pages/about.js
@@ -39,11 +39,12 @@ const About = () => {
         <title>Portfolio | About Page</title>
         <meta name="description" content="any description" />
       </Head>
-      <main className="w-full flex flex-col items-center justify-center mb-16 overflow-hidden dark:text-light 2x1:">
+      <main className="w-full flex flex-col items-center justify-center mb-16 overflow-hidden dark:text-light">
         <Layout className="pt-16">
-          <AnimatedText text="Passion Fuels Purpose! " className="mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl" />
-          <div className="grid w-full grid-cols-8 gap-32">
-            <div className="col-span-3 flex flex-col items-start justify-start">
+          <AnimatedText text="Passion Fuels Purpose! "
+                        className="mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8" />
+          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8">
+            <div className="col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 md:col-span-8">
               <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">Biography</h2>
               <p className="font-medium">
                 - Hi, I&apos;m Svetlana, a web developer with a passion for creating beautiful, functional,
@@ -65,39 +66,42 @@ const About = () => {
                 to bring my skills and passion to your next project.
               </p>
             </div>
+
             <div className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark
-            bg-light p-8 dark:bg-dark dark:border-light">
+            bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8">
+
               <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light" />
               <Image src={profile} alt="Svetlana" className="w-full h-auto rounded-2xl"
                      priority
-                     sizes="(max-width: 768px) 100vw,
-              (max-width: 1200px) 50vw,
-              33vw"
+                     sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               />
-
             </div>
 
-
-            <div className="col-span-2 flex flex-col item-end justify-between">
-              <div className="flex flex-col items-end justify-center">
-                <span className="inline-block text-7xl font-bold">
+            <div
+              className="col-span-2 flex flex-col item-end justify-between xl:col-span-8 xl:flex-row xl:items-center md:order-3">
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
                   <AnimatedNumbers value={5} />+
                 </span>
-                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light">satisfied clients</h2>
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center
+                md:text-lg sm:text-base xs:text-sm
+                ">satisfied clients</h2>
               </div>
 
-              <div className="flex flex-col items-end justify-center">
-                <span className="inline-block text-7xl font-bold">
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
                   <AnimatedNumbers value={10} />+
                 </span>
-                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light">projects completed</h2>
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center
+                md:text-lg sm:text-base xs:text-sm">projects completed</h2>
               </div>
 
-              <div className="flex flex-col items-end justify-center">
-                <span className="inline-block text-7xl font-bold">
+              <div className="flex flex-col items-end justify-center xl:items-center">
+                <span className="inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl">
                   <AnimatedNumbers value={1} />+
                 </span>
-                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light">year of experience</h2>
+                <h2 className="text-xl font-medium capitalize text-dark/75 dark:text-light/75 xl:text-center
+                md:text-lg sm:text-base xs:text-sm">year of experience</h2>
               </div>
             </div>
 
