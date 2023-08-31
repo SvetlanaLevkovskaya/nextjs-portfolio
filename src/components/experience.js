3f91ac7f830623a9834c1dd6accbb2263567e51c
Svetlana Levkovskaya <levkovskayase@gmail.com>
2023-08-31 20:07:27 +0300

    feat: add responsiveness to education component

--- src/components/experience.js
+++ src/components/experience.js
@@ -37,12 +37,12 @@ export const Experience = () => {
 
   return (
     <div className="my-64">
-      <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:md--16">Experience</h2>
+      <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:md-16">Experience</h2>
 
       <div ref={ref} className="w-[75%] mx-auto relative lg:w-[90%] md:w-full">
         <motion.div
           style={{scaleY: scrollYProgress}}
-          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light md:W-[2px] md:left-[30px]
+          className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top dark:bg-light md:w-[2px] md:left-[30px]
           xs:left-[20px]
           " />
 
